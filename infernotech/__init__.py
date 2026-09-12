8a1c6bea9ea0e0f35bb2ca03f828870f8943a816
dominionochai <dominionchukwuonwe@gmail.com>
2026-09-12 12:03:36 +0100

    add video pipeline (fly-visual)

--- infernotech/__init__.py
+++ infernotech/__init__.py
@@ -3,6 +3,9 @@
 from .events import Event, coerce_events, load_events
 from .features import extract_features
 from .classifier import FlickerClassifier, FlickerDetection
+from .video_events import PseudoEventGenerator, PseudoEvents
+from .video_features import extract_region_features
+from .video_classifier import RuleClassifier, TwoStateHMM
 
 __all__ = [
     "Event",
@@ -11,4 +14,9 @@ __all__ = [
     "extract_features",
     "FlickerClassifier",
     "FlickerDetection",
+    "PseudoEventGenerator",
+    "PseudoEvents",
+    "extract_region_features",
+    "RuleClassifier",
+    "TwoStateHMM",
 ]
